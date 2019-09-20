a35e27ead92ced0eec47641baaed4999b3b8d5e2
Igor Gomes <51889513+wizardigor@users.noreply.github.com>
2019-09-19 22:59:36 -0300

    Update ContaBanco.dart
    Comentarios sobre as linhas de codigo.

--- ContaBanco.dart
+++ ContaBanco.dart
@@ -1,51 +1,53 @@
+//criação da classe Conta.
 class Conta{
+  //atributos da classe Conta.
   double chequeEspecial = 0;
   int numConta = 0;
   String titular = '';
   double saldo = 0;
   
- Conta(String titular, int conta){
+  //contruto da classe com atribuição direta.
+  Conta(String titular, int conta){
     this.titular = titular;
     this.numConta = conta;
     this.chequeEspecial = 200;
     print("CONTA CRIADA COM SUCESSO.\n${getImprimirSaldo()}");
   }
   
+  //atribuindo nome ao Titular da conta
   void setTitular(String titular){
     this.titular= titular;
   }
   
+  //atribuindo um nunero a conta
   void setConta(int conta){
     this.numConta = conta;
   }
   
+  //saque de valor com verificação sobre o saldo e cheque especial
   void setSaque(double saque){
     if (chequeEspecial <= 0){
-      print("SALDO EM CONTA INSUFICIENTE!\nTENTE UM VALOR 
-
-MENOR OU CONFIRA SEU SALDO.\n");
+      print("SALDO EM CONTA INSUFICIENTE!\nTENTE UM VALOR MENOR OU CONFIRA SEU SALDO.\n");
     }
     else if ((saldo - saque) >= 0){
       this.saldo-= saque;
       print("${getImprimirSaldo()}");
     }
-    else if(((saldo - saque) <= 0) && ((chequeEspecial - 
-
-(saldo - saque)) >= chequeEspecial )){
+    else if(((saldo - saque) <= 0) && ((chequeEspecial - (saldo - saque)) >= chequeEspecial )){
       this.chequeEspecial += (saldo - saque);
       this.saldo=0;
       print("${getImprimirSaldo()}");
     }
   }
   
+  //insere o vador de deposito direto ao saldo.
   void setDeposito (double deposito){
     this.saldo += deposito;
   }
   
+  //mostra na tela o saldo atual e ovalor do cheque especial.
   String getImprimirSaldo(){
-      return "SALDO ATUAL: R\$ $saldo\nVOCÊ TEM: R\$ 
-
-$chequeEspecial EM CHEQUE ESPECIAL.\n";   
+      return "SALDO ATUAL: R\$ $saldo\nVOCÊ TEM: R\$ $chequeEspecial EM CHEQUE ESPECIAL.\n";   
   } 
 }
 
